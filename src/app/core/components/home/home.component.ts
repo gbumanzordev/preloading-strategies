4b9e2199d63d0469a253c5556eb8dfac6b8e2b61
GersonUmanzor <gbumanzor@outlook.com>
2021-04-18 16:45:48 -0600

    Add DOM events to handle on-demand preload.

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { OnDemandPreloadService } from '../../strategies/on-demand-preload.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private onDemandPreloadService: OnDemandPreloadService) {}
+
+  preloadAll(): void {
+    this.onDemandPreloadService.startPreload('*');
+  }
+}
--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { OnDemandPreloadService } from '../../strategies/on-demand-preload.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(private onDemandPreloadService: OnDemandPreloadService) {}
+
+  preloadChunk(url: string): void {
+    this.onDemandPreloadService.startPreload(url);
+  }
+}
