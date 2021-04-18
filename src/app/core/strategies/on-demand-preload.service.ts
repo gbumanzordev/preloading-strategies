747e10c471dd2bb7b5eb058affa4d1d10cfea64e
GersonUmanzor <gbumanzor@outlook.com>
2021-04-18 16:42:31 -0600

    Create service to load on-demand chunks.

---
+++ src/app/core/strategies/on-demand-preload.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+export class OnDemandPreloadOptions {
+  constructor(public routePath: string, public preload = true) {}
+}
+
+@Injectable({ providedIn: 'root' })
+export class OnDemandPreloadService {
+  private subject = new Subject<OnDemandPreloadOptions>();
+
+  state = this.subject.asObservable();
+
+  startPreload(routePath: string): void {
+    const message = new OnDemandPreloadOptions(routePath, true);
+    this.subject.next(message);
+  }
+}
