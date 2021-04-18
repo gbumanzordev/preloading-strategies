72338664771642d2ce355df1f2fa1aeb97ce40d1
GersonUmanzor <gbumanzor@outlook.com>
2021-04-18 16:19:12 -0600

    Import core module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
