2898c1eac625ab28c9cb7ce19acd46df00ef1c9f
GersonUmanzor <gbumanzor@outlook.com>
2021-04-18 15:57:13 -0600

    Create interface for users.

---
+++ src/app/users/interfaces/user.interface.ts
@@ -0,0 +1,27 @@
+export interface IUser {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: IAddress;
+  phone: string;
+  website: string;
+  company: ICompany;
+}
+
+export interface IAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
+
+export interface ICompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
